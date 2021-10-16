bda8882d8bf6cd7754559f2b01c6e6f9ada60eb9
hsbmaulana <hsbmaulana.iso@protonmail.com>
2021-10-16 21:30:12 +0700

    Major

---
+++ app/Providers/EventServiceProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Event;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * @var array
+     */
+    protected $listen = [];
+
+    /**
+     * @return void
+     */
+    // public function register() {}
+
+    /**
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        Event::listen(function (\Illuminate\Notifications\Events\NotificationSent $notif) {
+
+            if ($notif->channel === 'database' && method_exists($notif->notification, 'broadcastType')) {
+
+                $notif->response->type = $notif->notification->broadcastType();
+                $notif->response->save();
+            }
+        });
+    }
+
+    /**
+     * @return bool
+     */
+    public function shouldDiscoverEvents()
+    {
+        return false;
+    }
+}
